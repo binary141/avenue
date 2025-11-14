39f96316613424397df96d7bfae06554eb55eb22
Kaleb Petersen <kcpetersen111@gmail.com>
2025-11-14 10:59:44 -0700

    better res

--- backend/handlers/fileupload.go
+++ backend/handlers/fileupload.go
@@ -10,30 +10,40 @@ type UploadReq struct {
 	Extension string `json:"extension"  binding:"required"`
 	Data      string `json:"data" binding:"required"`
 }
+type Response struct {
+	Message string `json:"message"`
+	Error   string `json:"error"`
+}
 
 func (s *Server) Upload(c *gin.Context) {
 	var req UploadReq
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(400, gin.H{
-			"message": "could not marshal all data to json",
-			"error":   err.Error(),
+		c.JSON(400, Response{
+			Message: "could not marshal all data to json",
+			Error:   err.Error(),
 		})
+		return
 	}
 	fs := afero.NewOsFs()
 	f, err := fs.Create("temp/test.txt")
 	if err != nil {
-		c.JSON(500, gin.H{
-			"message": "could not create file",
-			"error":   err.Error(),
+		c.JSON(500, Response{
+			Message: "could not create file",
+			Error:   err.Error(),
 		})
+		return
 	}
 	defer f.Close()
 	_, err = f.Write([]byte(req.Data))
 	if err != nil {
-		c.JSON(500, gin.H{
-			"message": "could not write to file",
-			"error":   err.Error(),
+		c.JSON(500, Response{
+			Message: "could not write to file",
+			Error:   err.Error(),
 		})
+		return
 	}
-	// afero.WriteFile(os.Stdout, []byte(req.Data), 0644)
+	c.JSON(200, Response{
+		Message: "file uploaded successfully",
+		Error:   "",
+	})
 }

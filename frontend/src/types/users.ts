f138130d184dc82f08662d78ca59f961e5017b06
Brendan Davidson <binary141@gmail.com>
2025-11-30 21:21:41 -0700

    Update the user type

--- frontend/src/types/users.ts
+++ frontend/src/types/users.ts
@@ -1,3 +1,10 @@
 export interface User {
+    canLogin: boolean;
+    createdAt: string;
+    deletedAt: string | null;
+    email: string;
+    firstName: string | null;
     id: number;
+    lastName: string | null;
+    updatedAt: string | null;
 }
